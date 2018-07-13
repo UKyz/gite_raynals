894c6170fb3c868a6823c5806c452a11f84f71d3
UKyz <ukyz@icloud.com>
2018-07-13 11:48:31 +0200

    add some in gitignore

--- config/config_init.php
+++ config/config_init.php
@@ -26,9 +26,9 @@ try {
     if ($_SERVER['SERVER_NAME'] == "localhost" OR $_SERVER['SERVER_NAME'] == "sikia.synology.me"
         OR $_SERVER['SERVER_NAME'] == "192.168.1.59") {
         $host = "localhost";
-        $dbname = "domaine_les_reynals";
+        $dbname = "gite_raynales";
         $dbuser = "root";
-        $dbmdp = "admin";
+        $dbmdp = "root";
         $dbport = "3306";
     } else {
 //        $host = "db474181553.db.1and1.com";
